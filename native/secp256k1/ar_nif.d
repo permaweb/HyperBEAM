0f91990aa2c78aad8b812c220f862b509d22d443
Sam Williams <sam@arweave.org>
2025-04-17 20:54:07 -0400

    wip: port Arweave secp256k1 NIF

---
+++ native/secp256k1/ar_nif.d
@@ -0,0 +1,5 @@
+native/secp256k1/ar_nif.o: native/secp256k1/ar_nif.c \
+  native/secp256k1/ar_nif.h /usr/local/lib/erlang/usr/include/erl_nif.h \
+  /usr/local/lib/erlang/usr/include/erl_drv_nif.h \
+  /usr/local/lib/erlang/usr/include/erl_int_sizes_config.h \
+  /usr/local/lib/erlang/usr/include/erl_nif_api_funcs.h
