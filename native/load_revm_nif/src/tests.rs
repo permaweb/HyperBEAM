687e8539ee2823ed587b49b0fa4883dd0845c9a1
charmful0x <charmful0x@gmail.com>
2025-04-22 12:18:23 +0200

    feat: custom EVM -- block gas limit & contract size

--- native/load_revm_nif/src/tests.rs
+++ native/load_revm_nif/src/tests.rs
@@ -7,6 +7,18 @@ mod tests {
 
     #[test]
     // native gas token transfer
+    fn test_native_gas_token_transfer_chain_9496_new_state() {
+        let raw_tx_hex = "02f87782251881878447868c008447868c008307a12094c69b7ea1931e207bebe89fa32b10435aec234c40893635c9adc5dea0000080c080a07a207f6815ba6a9d92b49f205695514c93fbdb2abe37a1610389958a92f13aa4a0556295a23580307b7f0248784dcb9ec9c9c7aba9234703f902e0b37f227ab9f2";
+        let previous_state = None;
+        let (result, state) = eval(raw_tx_hex.to_string(), previous_state).unwrap();
+
+        let result_json: Value = serde_json::to_value(&result).unwrap();
+        println!("TX response:{:?}", result_json);
+        println!("new eval state result: {}", state);
+    }
+
+    #[test]
+    // native gas token transfer
     fn test_native_gas_token_transfer() {
         let raw_tx_hex = "02f87782251881878447868c008447868c008307a12094c69b7ea1931e207bebe89fa32b10435aec234c40893635c9adc5dea0000080c080a07a207f6815ba6a9d92b49f205695514c93fbdb2abe37a1610389958a92f13aa4a0556295a23580307b7f0248784dcb9ec9c9c7aba9234703f902e0b37f227ab9f2";
         let previous_state = fs::read_to_string("./state.json").unwrap();
