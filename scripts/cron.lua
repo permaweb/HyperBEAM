2ddf01fee9da5b914155f70a26731990f58f62d4
viksit <viksit@gmail.com>
2025-04-23 16:41:55 -0400

    wip exploring functionality

--- scripts/cron.lua
+++ scripts/cron.lua
@@ -1,11 +1,54 @@
-function handle(base, req, opts)
-    base.crons = base.crons or {}
-    if req.body.path == "once" then
-        table.insert(base.crons, req.body)
-    elseif req.body.path == "every" then
-        table.insert(base.crons, req.body)
-    elseif req.body.path == "stop" then
-        -- TODO: Stop the cron
-    end
+-- main handler for cron caching
+-- function handle(base, req, opts)
+-- 	ao.event("debug_cron", { "handle:base", base })
+-- 	ao.event("debug_cron", { "handle:req", req })
+-- 	ao.event("debug_cron", { "handle:opts", opts })
+--     base.crons = base.crons or {}
+--     if req.body.path == "once" then
+-- 		ao.event("debug_cron", { "handle:path:once", req.body.path })
+--         table.insert(base.crons, req.body)
+--     elseif req.body.path == "every" then
+-- 		ao.event("debug_cron", { "handle:path:every", req.body.path })
+--         table.insert(base.crons, req.body)
+--     elseif req.body.path == "stop" then
+-- 		ao.event("debug_cron", { "handle:path:stop", req.body.path })
+--         -- TODO: Stop the cron
+--     end
+-- 	base.crons = {
+-- 		output = {
+-- 			foo = 42
+-- 		}
+-- 	}
+--     return base
+-- end
+
+function compute(process, message, opts)
+	local res = 42
+	process.crons = {
+		body = res
+    }
+	ao.event("debug_cron", { "111compute:message", message.body })
+	-- ao.event("debug_cron", { "compute:opts", opts })
+	-- if message.body.body and message.body.body.body.path == "once" then
+	-- 	ao.event("debug_cron", { "log1", "1" })
+	-- 	-- process.crons = {
+	-- 	-- 	body = res + 1
+	-- 	-- }
+	-- else
+	-- 	ao.event("debug_cron", { "log2", "2" })
+	-- 	-- ao.event("debug_cron", { "2compute:message.body.body", message.body.body })
+	-- 	-- process.crons = {
+	-- 	-- 	body = res + 2
+	-- 	-- }
+	-- end
+    return process
+end
+
+-- test function for cache test in dev_cron.erl
+function hello(base, req, opts)
+	ao.event("debug_cron", { "base", base })
+	ao.event("debug_cron", { "req", req })
+	ao.event("debug_cron", { "opts", opts })
+    base.hello = req.name or "world"
     return base
 end
