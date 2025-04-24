0132c5fa8e6557d61fe7aa0674702b7568770df8
viksit <viksit@gmail.com>
2025-04-24 11:48:27 -0400

    clean up experimental code

--- scripts/cron.lua
+++ scripts/cron.lua
@@ -18,7 +18,7 @@ function compute(process, message, opts)
         table.insert(process.crons, message.body)
         
     elseif command == "remove" then
-		-- TODO(viksit): this also needs to be called when a process is stopped. 
+		-- TODO(viksit): this also needs to be called when a cron job ends.
 		-- how do we do that?
         -- Remove an entry by task_id
         local task_id = message.body.body.task_id
@@ -41,64 +41,4 @@ function compute(process, message, opts)
     end
     
     return process
-end
-
-
-
--- Experimental code, to be cleaned up
-
---(currently ignored) main handler for cron caching
-function handle(base, req, opts)
-	ao.event("debug_cron", { "handle:base", base })
-	ao.event("debug_cron", { "handle:req", req })
-	ao.event("debug_cron", { "handle:opts", opts })
-	base.crons = base.crons or {}
-	if req.body.path == "once" then
-		ao.event("debug_cron", { "handle:path:once", req.body.path })
-		table.insert(base.crons, req.body)
-	elseif req.body.path == "every" then
-		ao.event("debug_cron", { "handle:path:every", req.body.path })
-		table.insert(base.crons, req.body)
-	elseif req.body.path == "stop" then
-		ao.event("debug_cron", { "handle:path:stop", req.body.path })
-		-- TODO(viksit): remove the cron from the table
-	end
-	base.crons = {
-		output = {
-			foo = 42
-		}
-	}
-	return base
-end
--- (currently ignored) original compute function
-function computeOld(process, message, opts)
-	-- early return when no body is provided
-	-- for some reason, the function is call twice, once without params
-	-- likely during schedule.
-	if not message.body.body then
-		return process
-	end
-	-- main logic to initialize the crons
-	process.crons = process.crons or {}
-	if message.body.body.path == "once" then
-		ao.event("debug_cron", { "add once process", "1" })
-		table.insert(process.crons, message.body)
-	elseif message.body.body.path == "every" then
-		ao.event("debug_cron", { "add every process", "2" })
-		table.insert(process.crons, message.body)
-	elseif message.body.body.path == "stop" then
-		ao.event("debug_cron", { "stop process", "3" })
-		-- TODO: remove the cron from the table (needs table for loop)
-		-- table.remove(process.crons, message.body)
-	end
-	return process
-end
-
--- test function for cache test in dev_cron.erl
-function hello(base, req, opts)
-	ao.event("debug_cron", { "base", base })
-	ao.event("debug_cron", { "req", req })
-	ao.event("debug_cron", { "opts", opts })
-    base.hello = req.name or "world"
-    return base
 end
