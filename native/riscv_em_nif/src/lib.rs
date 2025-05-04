2b00146f3e3de67c890f8ff14b549ca0f937d561
charmful0x <charmful0x@gmail.com>
2025-05-05 00:37:36 +0200

    feat: mvp-ready eval_riscv_bytecode

--- native/riscv_em_nif/src/lib.rs
+++ native/riscv_em_nif/src/lib.rs
@@ -4,8 +4,7 @@ pub mod utils;
 #[cfg(test)]
 pub mod tests;
 
-use core::state::{deserialize_state, serialize_state, get_state};
-
+use core::state::{deserialize_state, serialize_state, get_state, shallow_merge, get_base_path};
 use crate::core::riscv_machine::evaluate_raw_tx;
 use rustler::NifResult;
 
@@ -21,10 +20,23 @@ fn hello() -> NifResult<String> {
 }
 
 #[rustler::nif]
-fn eval_riscv_bytecode(signed_raw_tx: String, state: String) -> NifResult<String> {
+fn eval_riscv_bytecode(signed_raw_tx: String, chain_id: String) -> NifResult<String> {
+    let state = get_state(&chain_id);
+    
+    if state.len() == 0 {
+            return Ok(serde_json::json!({"error": "invalid chain_id"}).to_string())
+        };
+
     let deserialized_state = deserialize_state(&state).map_err(|_| rustler::Error::Atom("Error deserializing json state"))?;
     let evaluation_res = evaluate_raw_tx(deserialized_state, &signed_raw_tx);
     let serialized_evaluated_state = serialize_state(evaluation_res.0.accounts).map_err(|_| rustler::Error::Atom("Error evaluating calldata"))?;
+    let merged_states = shallow_merge(&state, &serialized_evaluated_state).map_err(|_| rustler::Error::Atom("Error shallow merging states"))?;
+    
+    if merged_states.1 {
+        let path = format!("{}/{}.json", get_base_path("appchains"), chain_id);
+        let _state_update = std::fs::write(path, merged_states.0).map_err(|_| rustler::Error::Atom("Error saving new state"))?;;
+    }
+
     Ok(serialized_evaluated_state)
 }
 
