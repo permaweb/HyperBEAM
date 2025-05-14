e4318341e16c4de0cbff91919c9af45209be634d
Elliot Sayes <elliot@sayes.cloud>
2025-05-14 15:38:48 +0200

    More test coverage for emscripten exceptions

--- src/dev_emscripten.erl
+++ src/dev_emscripten.erl
@@ -19,13 +19,13 @@
 %%% 
 %%% Where '_vjj' represents the type spec of the function.
 -module(dev_emscripten).
--export([info/1, init/3, '_emscripten_memcpy_js'/3, '__cxa_throw'/3, '_emscripten_throw_longjmp'/3, invoke_ii/3, invoke_vjj/3, invoke_viii/3, router/4, emscripten_date_now/3]).
+-export([info/1, init/3, '_emscripten_memcpy_js'/3, '__cxa_throw'/3, '__cxa_rethrow'/3, '_emscripten_throw_longjmp'/3, invoke_v/3, invoke_ii/3, invoke_jj/3, invoke_vjj/3, invoke_vjjj/3, invoke_viii/3, router/4, emscripten_date_now/3]).
 
 
 -include("src/include/hb.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
-% -hb_debug(print).
+-hb_debug(print).
 
 info(_) ->
     #{
@@ -63,14 +63,31 @@ init(M1, _M2, Opts) ->
     ?event(Res),
     Res.
 
+'__cxa_rethrow'(_Msg1, _Msg2, _Opts) ->
+    Res = {import_exception, <<"__cxa_throw">>},
+    ?event(Res),
+    Res.
+
 '_emscripten_throw_longjmp'(_Msg1, _Msg2, _Opts) ->
     Res = {import_exception, <<"_emscripten_throw_longjmp">>},
     ?event(Res),
     Res.
 
+invoke_v(Msg1, Msg2, Opts) ->
+	?event(invoke_emscripten_v),
+	router(<<"invoke_v">>, Msg1, Msg2, Opts).
+
 invoke_ii(Msg1, Msg2, Opts) ->
 	?event(invoke_emscripten_ii),
 	router(<<"invoke_ii">>, Msg1, Msg2, Opts).
+
+invoke_jj(Msg1, Msg2, Opts) ->
+	?event(invoke_emscripten_jj),
+	router(<<"invoke_jj">>, Msg1, Msg2, Opts).
+
+invoke_vjjj(Msg1, Msg2, Opts) ->
+	?event(invoke_emscripten_vjjj),
+	router(<<"invoke_vjjj">>, Msg1, Msg2, Opts).
 
 invoke_vjj(Msg1, Msg2, Opts) ->
 	?event(invoke_emscripten_vjj),
@@ -84,14 +101,20 @@ router(<<"invoke_", Sig/binary>>, Msg1, Msg2, Opts) ->
     ?event(invoke_emscripten),
     State = hb_ao:get(<<"state">>, Msg1, #{ hashpath => ignore }),
     WASM = dev_wasm:instance(State, Msg2, Opts),
-    [Index|Args] = hb_ao:get(args, Msg2, #{ hashpath => ignore }),
+    [Index|ArgsRaw] = hb_ao:get(args, Msg2, #{ hashpath => ignore }),
+    % If Args is a list, keep it, otherwise wrap it in one
+    Args = case is_list(ArgsRaw) of
+        true -> ArgsRaw;
+        false -> [ArgsRaw]
+    end,
     ?event(debug, {invoke, {invoke_signature, Sig}, {indirect_function_index, Index}, {args, Args}}),
     ?event(debug, invoke_emscripten_stack_get_current),
     {ok, [SP]} = dev_wasm:call(WASM, <<"emscripten_stack_get_current">>, []),
     ?event(debug, {invoke_stack_pointer, SP}),
+    ?event({calling_import_resolver, {state, State, opts, Opts}}),
     ImportResolver = hb_private:get(<<"wasm/import-resolver">>, State, Opts),
     try
-        ?event(trying_indirect_call),
+        ?event({trying_indirect_call, {index, Index}, {args, Args}}),
         Res = dev_wasm:call(WASM, Index, Args, ImportResolver, State, Opts),
         {ok, Result, StateMsg} = Res,
         ?event(debug, {try_indirect_call_succeeded, Result}),
@@ -159,14 +182,17 @@ fib_test() ->
     {ok, Output} = hb_wtime_io:read_string(Instance, Ptr),
     ?assertEqual(<<"Fibonacci index 10 is 55\n">>, Output).
 
-%% @doc Ensure that an AOS Emscripten-style WASM AOT module can be invoked
-%% with a function reference.
-try_test() ->
-    Init = generate_emscripten_stack("test/try.wasm", <<"handle">>, [0, 0]),
+% 0 - No catch cause triggered
+% 1 - `throw` exception is caught
+% 2 - `throw(int)` exception is caught
+% 3 - `throw(runtime_error)` exception is caught
+% 4 - native dereferencing an invalid pointer cannot be caught
+try_test_case(Mod, N) -> 
+    Init = generate_emscripten_stack(Mod, <<"handle">>, [0, 0]),
     Instance = hb_private:get(<<"wasm/instance">>, Init, #{}),
     Msg = <<"msg">>,
-    % Note: set to <<"1">> to enable the try/catch path
-    Env = <<"1">>,
+    % int to binary
+    Env = list_to_binary(integer_to_list(N)),
     {ok, Ptr1} = hb_wtime_io:malloc(Instance, byte_size(Msg)),
     ?assertNotEqual(0, Ptr1),
     hb_wtime:mem_write(Instance, Ptr1, Msg),
@@ -177,8 +203,54 @@ try_test() ->
     {ok, StateRes} = hb_ao:resolve(Ready, <<"compute">>, #{}),
     [Ptr] = hb_ao:get(<<"results/wasm/output">>, StateRes),
     {ok, Output} = hb_wtime_io:read_string(Instance, Ptr),
-    ?assertEqual(<<"Catch">>, Output).
+    Output.
 
+try_nothing_test() ->
+    Output32 = try_test_case("test/try.wasm", 0),
+    ?assertEqual(<<"Initial">>, Output32),
+    Output64 = try_test_case("test/try-64.wasm", 0),
+    ?assertEqual(<<"Initial">>, Output64).
+
+% `throw` exception is caught
+try_throw_test() ->
+    Output32 = try_test_case("test/try.wasm", 1),
+    ?assertEqual(<<"Catch">>, Output32),
+    Output64 = try_test_case("test/try-64.wasm", 1),
+    ?assertEqual(<<"Catch">>, Output64).
+
+% `throw(int)` exception is caught
+try_throw_int_test() ->
+    % Output32 = try_test_case("test/try.wasm", 2),
+    % ?assertEqual(<<"Catch">>, Output32),
+    Output64 = try_test_case("test/try-64.wasm", 2),
+    ?assertEqual(<<"Catch">>, Output64).
+
+% `throw(runtime_error)` exception is caught
+try_throw_runtime_error_test() ->
+    Output32 = try_test_case("test/try.wasm", 3),
+    ?assertEqual(<<"Catch">>, Output32),
+    Output64 = try_test_case("test/try-64.wasm", 3),
+    ?assertEqual(<<"Catch">>, Output64).
+
+% native dereferencing an invalid pointer cannot be caught
+try_throw_invalid_pointer_test() ->
+    try
+        try_test_case("test/try.wasm", 4)
+    catch
+        _:Error ->
+            {badmatch, {error, {call_begin_failed, ErrorStrBin}}} = Error,
+            ?event(debug, {invoke_try_error, ErrorStrBin})
+    end,
+    try
+        try_test_case("test/try-64.wasm", 4)
+    catch
+        _:Error2 ->
+            {badmatch, {error, {call_begin_failed, ErrorStrBin2}}} = Error2,
+            ?event(debug, {invoke_try_error, ErrorStrBin2})
+    end.
+
+%% @doc Ensure that an AOS Emscripten-style WASM AOT module can be invoked
+%% with a function reference.
 basic_aos_exec_test_() ->
     {timeout, 30, fun () ->
         Init = generate_emscripten_stack("test/aos-new.wasm", <<"handle">>, []),
@@ -204,14 +276,14 @@ basic_aos_exec_test_() ->
         ?event({got_output, Output}),
         #{ <<"response">> := #{ <<"Output">> := #{ <<"data">> := Data }} }
             = hb_json:decode(Output),
-        DataStartExpected = <<"\e[90mNew Message From \e[32mFOO">>,
+        DataStartExpected = <<"\e[90mNew Message From \e[32mFB.">>,
         DataStart = binary:part(Data, 0, byte_size(DataStartExpected)),
         ?assertEqual(DataStartExpected, DataStart)
     end}.
 
 %%% Test Helpers
 gen_test_env() ->
-    <<"{\"Process\":{\"Id\":\"AOS\",\"Owner\":\"FOOBAR\",\"Tags\":[{\"name\":\"Name\",\"value\":\"Thomas\"}, {\"name\":\"Authority\",\"value\":\"FOOBAR\"}]}}\0">>.
+    <<"{\"Process\":{\"Id\":\"AOS\",\"Owner\":\"FB\",\"Tags\":[{\"name\":\"Name\",\"value\":\"Thomas\"}, {\"name\":\"Authority\",\"value\":\"FOOBAR\"}]}}\0">>.
 
 gen_test_aos_msg(Command) ->
-    <<"{\"From\":\"FOOBAR\",\"Block-Height\":\"1\",\"Target\":\"AOS\",\"Owner\":\"FOOBAR\",\"Id\":\"1\",\"Module\":\"W\",\"Tags\":[{\"name\":\"Action\",\"value\":\"Eval\"}],\"Data\":\"", (list_to_binary(Command))/binary, "\"}\0">>.
+    <<"{\"From\":\"FB\",\"Block-Height\":\"1\",\"Target\":\"AOS\",\"Owner\":\"FB\",\"Id\":\"1\",\"Module\":\"W\",\"Tags\":[{\"name\":\"Action\",\"value\":\"Eval\"}],\"Data\":\"", (list_to_binary(Command))/binary, "\"}\0">>.
---
+++ test/try.cpp
@@ -0,0 +1,65 @@
+// #define LOG_ENABLED
+#include "log.h"
+
+#include <cstring>
+#include <stdexcept>
+
+char result_str[256] = "Initial";
+
+extern "C" char *handle(char *msg, char *env) {
+  LOG("printf: handle()\n");
+  LOG("msg: %s\n", msg);
+  LOG("env: %s\n", env);
+
+  // This triggers a bug in WAMR
+  // Calling `__wasm_call_ctors` before functions are linked
+  // cout << "cout handle()" << endl;
+
+  try {
+    LOG("Try\n");
+
+    // strcpy(result_str, "Try");
+    LOG("result_str: %s\n", result_str);
+
+    LOG("env[0]: %c\n", env[0]);
+
+    // int d = atoi(env);
+    if (env[0] == '1') {
+      LOG("About to `throw` ...\n");
+      throw; // gets converted to call __cxa_rethrow
+    }
+    else if (env[0] == '2') {
+      LOG("About to `throw(37703)` ...\n");
+      throw(37703); // gets converted to call __cxa_throw
+    }
+    else if (env[0] == '3') {
+      LOG("About to `throw(std::runtime_error(\"MyRuntimeError\"))` ...\n");
+      throw(std::runtime_error("MyRuntimeError"));
+    }
+    else if (env[0] == '4') {
+      int *x = (int *)0xFFFFFFFFFFFF; // 48-bit pointer
+      LOG("About to dereference invalid pointer (%p)...\n", x);
+      int val = *x;
+      if (val == 0) return NULL;
+      LOG("x: %d\n", val);
+    }
+
+    LOG("No Exception\n");
+  } catch (...) {
+    LOG("Exception caught\n");
+
+    strcpy(result_str, "Catch");
+    LOG("result_str: %s\n", result_str);
+
+    // LOG("e.what(): %s\n", e.what());
+  }
+
+  return result_str;
+}
+
+int main(int argc, char *argv[]) {
+  LOG("C++ - try/catch example\n");
+  handle(NULL, "0");
+  LOG("Done\n");
+  return 0;
+}
