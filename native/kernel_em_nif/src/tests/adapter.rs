aff2a41cf6a5208f126084a8f7384c83ab9e2ef5
charmful0x <charmful0x@gmail.com>
2025-05-16 01:26:24 +0200

    feat: adapter info

---
+++ native/kernel_em_nif/src/tests/adapter.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+
+    use crate::core::execution_machine::KernelExecutor;
+
+    #[test]
+
+fn test_adapter_info() {
+    let adapter = pollster::block_on(KernelExecutor::get_adapter_info());
+    println!("{}", adapter);
+}
+}
--- native/kernel_em_nif/src/tests/mod.rs
+++ native/kernel_em_nif/src/tests/mod.rs
@@ -1,1 +1,2 @@
 pub mod double;
+pub mod adapter;
