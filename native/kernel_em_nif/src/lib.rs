95ab718e9471478be71fe050e096415e0fa679c3
charmful0x <charmful0x@gmail.com>
2025-06-04 11:36:07 +0200

    chore: fmt

--- native/kernel_em_nif/src/lib.rs
+++ native/kernel_em_nif/src/lib.rs
@@ -1,15 +1,15 @@
 pub mod core;
-pub mod utils;
 #[cfg(test)]
 pub mod tests;
+pub mod utils;
 use crate::core::arweave::retrieve_kernel_src;
 use crate::core::execution_machine::KernelExecutor;
 use crate::utils::load0::upload_to_load0;
+use image::ImageBuffer;
+use image::ImageFormat;
+use image::Rgba;
 use image::codecs::png;
 use rustler::NifResult;
-use image::ImageBuffer;
-use image::Rgba;
-use image::ImageFormat;
 use std::io::Cursor;
 
 mod atoms {
@@ -31,7 +31,8 @@ fn execute_kernel(
 ) -> NifResult<Vec<u8>> {
     let kernel_src = retrieve_kernel_src(&kernel_id).unwrap();
     let kem = pollster::block_on(KernelExecutor::new());
-    let result = kem.execute_kernel_default(&kernel_src, input_data.as_slice(), Some(output_size_hint));
+    let result =
+        kem.execute_kernel_default(&kernel_src, input_data.as_slice(), Some(output_size_hint));
     Ok(result)
 }
 
@@ -43,38 +44,40 @@ fn execute_kernel_with_params(
     input_data: rustler::Binary,
     params: rustler::Binary,
 ) -> NifResult<String> {
+    let img = image::load_from_memory(&input_data).unwrap();
 
-	let img = image::load_from_memory(&input_data).unwrap();
+    // let img = if img.width() > 2048 || img.height() > 2048 {
+    // 	img.resize(2048, 2048, image::imageops::FilterType::Lanczos3)
+    // } else {
+    // 	img
+    // };
 
-	// let img = if img.width() > 2048 || img.height() > 2048 {
-	// 	img.resize(2048, 2048, image::imageops::FilterType::Lanczos3)
-	// } else {
-	// 	img
-	// };
+    let rgba_img = img.to_rgba8();
+    let (width, height) = rgba_img.dimensions();
+    let pixel_bytes = rgba_img.into_raw();
 
-	let rgba_img = img.to_rgba8();
-	let (width, height) = rgba_img.dimensions();
-	let pixel_bytes = rgba_img.into_raw();
+    let pixel_size = if params.len() >= 12 {
+        u32::from_le_bytes([params[8], params[9], params[10], params[11]])
+    } else {
+        50 // default pixel size
+    };
+    let kernel_src = retrieve_kernel_src(&kernel_id).unwrap();
+    let kem = pollster::block_on(KernelExecutor::new());
+    let result = kem.execute_kernel_with_uniform_default(
+        &kernel_src,
+        pixel_bytes.as_slice(),
+        params.as_slice(),
+    );
 
-	let pixel_size = if params.len() >= 12 {
-		u32::from_le_bytes([params[8], params[9], params[10], params[11]])
-	} else {
-		50 // default pixel size
-	};
-	let kernel_src = retrieve_kernel_src(&kernel_id).unwrap();
-	let kem = pollster::block_on(KernelExecutor::new());
-	let result = kem.execute_kernel_with_uniform_default(&kernel_src, pixel_bytes.as_slice(), params.as_slice());
+    let output_data = &result[0..pixel_bytes.len()];
 
-	let output_data = &result[0..pixel_bytes.len()];
+    let output_img =
+        ImageBuffer::<Rgba<u8>, Vec<u8>>::from_raw(width, height, output_data.to_vec()).unwrap();
 
-	let output_img = ImageBuffer::<Rgba<u8>, Vec<u8>>::from_raw(
-		width, 
-		height, 
-		output_data.to_vec()
-	).unwrap();
-
-	let mut png_bytes = Vec::new();
-    output_img.write_to(&mut Cursor::new(&mut png_bytes), ImageFormat::Png).unwrap();
+    let mut png_bytes = Vec::new();
+    output_img
+        .write_to(&mut Cursor::new(&mut png_bytes), ImageFormat::Png)
+        .unwrap();
     let load0_hash = upload_to_load0(png_bytes, "image/png").unwrap();
     Ok(load0_hash)
 }
@@ -85,4 +88,12 @@ fn adapter_info() -> NifResult<String> {
     Ok(adapter)
 }
 
-rustler::init!("kem_nif", [hello, execute_kernel, execute_kernel_with_params, adapter_info]);
+rustler::init!(
+    "kem_nif",
+    [
+        hello,
+        execute_kernel,
+        execute_kernel_with_params,
+        adapter_info
+    ]
+);
