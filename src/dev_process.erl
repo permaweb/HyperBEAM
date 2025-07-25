c7a18f695f05e5bc6b2cb19ad12b973897673912
Jack Frain <jfrain99@gmail.com>
2025-07-25 11:26:15 -0400

    fix: revert dev_process change causing infinite loop of links

--- src/dev_process.erl
+++ src/dev_process.erl
@@ -697,8 +697,7 @@ run_as(Key, Base, Req, Opts) ->
     % This sets up the device context for the specific operation type.
     PreparedMsg =
         hb_util:deep_merge(
-    % POINT OF FAILURE
-            ensure_process_key(hb_message:normalize_commitments(Msg1, Opts), Opts),
+            ensure_process_key(Msg1, Opts),
             #{
                 <<"device">> =>
                     DeviceSet =
