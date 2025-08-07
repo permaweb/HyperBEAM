0d96d0b31e775a50974009be38810e24a2c6aea2
Jack Frain <jfrain99@gmail.com>
2025-08-06 21:37:54 -0400

    fix(wip): removed enoent clause from hb_store_fs

--- src/dev_process.erl
+++ src/dev_process.erl
@@ -550,7 +550,11 @@ now(RawBase, Req, Opts) ->
                     ?event(compute_short,
                         {serving_latest_cached_state,
                             {proc_id, ProcessID},
-                            {slot, LatestSlot}
+                            {slot, LatestSlot},
+                            {
+                                loaded_latest_msg, 
+                                hb_cache:ensure_all_loaded(LatestMsg, Opts)
+                            }
                         },
                         Opts
                     ),
