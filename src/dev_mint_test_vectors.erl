61a3a8a9b9deddd574a74c51727ccf43959678fb
Sam Williams <sam@arweave.org>
2025-11-01 12:25:27 -0400

    wip: additional test framework improvements

--- src/dev_mint_test_vectors.erl
+++ src/dev_mint_test_vectors.erl
@@ -8,48 +8,91 @@
 
 %%% Test Utilities
 
-%% @doc Return a random resource ID in human-readable (43-character) format.
+%% @doc Return random ID(s) in human-readable (43-character) format.
 random_id() -> hb_util:human_id(crypto:strong_rand_bytes(32)).
+random_ids(N) -> [ random_id() || _ <- lists:seq(1, N) ].
 
 %% @doc Generate a random resource with a random weight and oracle ID.
-random_resource(Max) ->
-    random_resource(random_id(), Max).
-random_resource(ID, Weight) when is_integer(Weight) ->
-    random_resource(ID, #{ <<"weight">> => Weight });
-random_resource(ID, Res) ->
+generate_resource(Max) ->
+    generate_resource(random_id(), Max).
+generate_resource(ID, undefined) ->
+    generate_resource(ID, rand:uniform(?DEFAULT_MAX_WEIGHT));
+generate_resource(ID, Weight) when is_integer(Weight) ->
+    generate_resource(ID, #{ <<"weight">> => Weight });
+generate_resource(ID, Res) ->
     {
         ID,
         Res#{
             <<"weight">> =>
                 hb_maps:get(
                     <<"weight">>,
-                    Res,
-                    rand:uniform(?DEFAULT_MAX_WEIGHT)
+                    Res
                 ),
             <<"oracle">> =>
                 hb_maps:get(<<"oracle">>, Res, random_id())
         }
     }.
 
-%% @doc Generate a set of random resources with random weights in given bounds.
-random_resources(N) ->
-    random_resources(N, ?DEFAULT_MAX_WEIGHT).
-random_resources(NumResources, Max) when is_integer(NumResources) ->
-    random_resources(
-        [ {random_id(), #{}} || _ <- lists:seq(1, NumResources) ],
-        Max
+%% @doc Generate a set of resources given a specification, either in the form of
+%% specific resources to employ, or a number of resources to generate, or a
+%% list of resource IDs.
+generate_resources(N) ->
+    generate_resources(N, undefined).
+generate_resources(NumResources, GlobalSpec) when is_integer(NumResources) ->
+    generate_resources(random_ids(NumResources), GlobalSpec);
+generate_resources(ResourceIDs, GlobalSpec) when is_list(ResourceIDs) ->
+    generate_resources(
+        hb_maps:from_list([ {ID, GlobalSpec} || ID <- ResourceIDs ]),
+        undefined
     );
-random_resources(Resources, Max) when is_map(Resources) ->
-    hb_maps:map(fun(ID, _) -> random_resource(ID, Max) end, Resources).
+generate_resources(Resources, GlobalSpec) when is_map(Resources) ->
+    hb_maps:map(
+        fun(ID, ResourceSpec) ->
+            element(2, generate_resource(
+                ID,
+                if is_map(ResourceSpec) andalso is_map(GlobalSpec) ->
+                    hb_maps:merge(GlobalSpec, ResourceSpec);
+                ResourceSpec =/= undefined ->
+                    ResourceSpec;
+                true ->
+                    GlobalSpec
+                end
+            ))
+        end,
+        Resources
+    ).
+
+%% @doc For each resource, generate a set of balances for each account.
+generate_all_balances(_, #{ balances := Balances }, _) ->
+    Balances;
+generate_all_balances(Resources, Params, Opts) ->
+    Accounts =
+        case maps:find(accounts, Params) of
+            {ok, AccountSpecs} -> AccountSpecs;
+            error -> random_ids(maps:get(account_count, Params, 10))
+        end,
+    hb_maps:map(
+        fun(_, _) ->
+            generate_balances(
+                Accounts,
+                maps:get(max_balance, Params, 1_000_000_000),
+                Opts
+            )
+        end,
+        Resources
+    ).
 
 %% @doc Generate a set of initial balances for a given set or number of accounts.
-initial_balances(AccountIDs, Max, Opts) when is_list(AccountIDs) ->
-    initial_balances(
+%% The resulting message may be used as a sub-message of the top-level balances
+%% message, attached to a specific resource ID. For example,
+%% `balances/resourceID/accountID`.
+generate_balances(AccountIDs, Max, Opts) when is_list(AccountIDs) ->
+    generate_balances(
         hb_maps:from_list([ {ID, #{}} || ID <- AccountIDs ]),
         Max,
         Opts
     );
-initial_balances(Accounts, Max, Opts) when is_map(Accounts) ->
+generate_balances(Accounts, Max, Opts) when is_map(Accounts) ->
     hb_ao:set(
         #{ <<"device">> => <<"trie@1.0">> },
         hb_maps:map(
@@ -93,45 +136,31 @@ initial_balances(Accounts, Max, Opts) when is_map(Accounts) ->
 %% - `cycle_proportion_denominator`: The denominator of the cycle proportion.
 %% By default, the returned state will mint 100% of its total supply of 1 token
 %% in a single cycle.
-initial_state(Params, Opts) ->
+generate_state(Params, Opts) ->
     Resources =
-        case maps:get(resources, Params, undefined) of
-            undefined ->
-                random_resources(maps:get(resource_count, Params, 2));
-            Res -> Res
-        end,
-    Balances = maps:get(balances, Params, #{}),
+        generate_resources(
+            maps:get(
+                resources,
+                Params,
+                maps:get(resource_count, Params, 2)
+            )
+        ),
+    Balances = generate_all_balances(Resources, Params, Opts),
     #{
         <<"device">> => <<"mint@1.0">>,
+        <<"cycle">> => maps:get(cycle, Params, 0),
         <<"cycle-proportion">> =>
             maps:get(cycle_proportion, Params, 1),
         <<"cycle-proportion-denominator">> =>
             maps:get(cycle_proportion_denominator, Params, 1),
-        <<"total-supply">> =>
+        <<"mint-total">> =>
             ?AO_TO_ARMS(maps:get(total_supply, Params, 1_000_000_000)),
+        <<"minted">> =>
+            ?AO_TO_ARMS(maps:get(minted, Params, 0)),
         <<"client">> =>
             maps:get(client, Params, random_id()),
         <<"resources">> => Resources,
-        <<"balances">> =>
-            hb_maps:map(
-                fun(ResID, _) ->
-                    case hb_maps:find(ResID, Balances, Opts) of
-                        {ok, ResourceBalances} ->
-                            ResourceBalances;
-                        error ->
-                            initial_balances(
-                                maps:get(
-                                    accounts,
-                                    Params,
-                                    maps:get(account_count, Params, 10)
-                                ),
-                                maps:get(max_balance, Params, 1_000_000_000),
-                                Opts
-                            )
-                    end
-                end,
-                Resources
-            ),
+        <<"balances">> => Balances,
         <<"total-supply">> => ?AO_TO_ARMS(1_000_000_000),
         <<"period">> => 1000,
         <<"start-time">> => os:system_time(millisecond)
@@ -140,11 +169,12 @@ initial_state(Params, Opts) ->
 %%% Tests
 
 single_resource_single_account_test() ->
+    hb:init(),
     AccID = random_id(),
     ResID = random_id(),
     {ok, NewS} =
         dev_mint_math:mint(
-            initial_state(
+            generate_state(
                 #{
                     resources => [ResID],
                     accounts => [AccID],
