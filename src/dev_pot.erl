0e6dc002f42c5d2e87829522cb671361e5e7e913
Sam Williams <sam@arweave.org>
2025-11-04 21:31:37 -0500

    feat: on-demand, real-time minting

--- src/dev_pot.erl
+++ src/dev_pot.erl
@@ -1,21 +1,23 @@
-%%% @doc An experimental real-time on-demand minting model, based largely on
-%%% notions from the MakerDAO DSR and MCD rate accumulation system.
+%%% @doc An experimental real-time on-demand minting model. Instead of minting
+%%% all tokens eagerly, this model mints tokens only on-demand. In doing so,
+%%% it significantly reduces the computational and message-passing complexity of the
+%%% system.
 %%% 
-%%% The theory is this:
-%%% 1. The period between changes in the state of deposits represent opportunities
-%%%    to calculate multiple yield periods in a single operation.
-%%% 2. Rather than eagerly at every mint timestep, yield accrual can be 
-%%%    calculated instead at the end of each stable period. Even though the rate
-%%%    of yield decays slightly per timestep, the summation of that yield should
-%%%    be nonetheless calculable efficiently.
-%%% 3. By normalizing balances against a common "chi" factor, all balances can
-%%%    be effectively updated by only updating one factor. Upon balance requests
-%%%    and token movements, the explicit balance per address can be renormalized.
-%%% 4. `Sub-mints' can be created by setting access rights on addresses proportionate
-%%%    to accrued yield during delegations to that address. For example, Alice
-%%%    may delegate her yield to ProjectA. While delegated, ProjectA receives
-%%%    the yield accrued from the `base' mint, while Alice's balance in the 
-%%%    `sub-mint' increases according to its separate rate.
+%%% h/t to MakerDAO's DSR and MCD rate accumulation system for some inspiration.
+%%% 
+%%% The core is this:
+%%% 1. Maintain a list of all balances for `resources` that lead to the minting
+%%%    of tokens.
+%%% 2. With each balance, store the `chi` factor at the time of creation.
+%%% 3. When the `drip` function is called, for each time-step since the last `drip`,
+%%%    calculate the yield that would have accrued to a balance holding one unit
+%%%    of the resource at that time: `rate(TimeStep) * (1/sum(deposits))`.
+%%% 4. When balances are requested or utilized, calculate the accrued yield by
+%%%    subtracting the current `chi` factor and the initial one. Multiply this
+%%%    by the number of units in the deposit. Count this with the existing reward
+%%%    balance: `total-balance = (chi - chi0) * deposit + existing-balance`.
+%%% 5. When the balance or deposit is modified in any way, first accrue the yield
+%%%    to the existing balance. Then perform the operation.
 -module(dev_pot).
 -include("include/hb.hrl").
 -include_lib("eunit/include/eunit.hrl").
@@ -38,12 +40,12 @@ core_test() ->
     report(S3),
     S4 = drip(S3#{ <<"t">> => 2 }),
     report(S4),
-    S5 = modify(Addr1, 3, S4),
+    S5 = modify(Addr1, 30, S4),
     S6 = drip(S5#{ <<"t">> => 3 }),
     report(S6),
-    S7 = modify(Addr1, -2, S6),
-    report(S7),
-    _S8 = drip(S7#{ <<"t">> => 4 }).
+    S7 = modify(Addr1, -30, S6),
+    S8 = drip(S7#{ <<"t">> => 4 }),
+    report(S8).
 
 report(S) ->
     ?event(
@@ -57,21 +59,18 @@ report(S) ->
 
 %%% Pot Model.
 
-pie(Human, Chi) -> Human / Chi.
-unpie(Normalized, Chi) -> Normalized * Chi.
-
 drip(S = #{ <<"t">> := T, <<"last-drip">> := Last }) when T =:= Last -> S;
 drip(S) ->
     % TODO: Can this be an integral of the rate function?
     drip(drip_once(S)).
 
 drip_once(S = #{ <<"chi">> := Chi, <<"last-drip">> := Last }) ->
-    NewChi = Chi * (1 + rate(S)),
+    NewChi = Chi + reward_per_unit(S),
     S#{ <<"chi">> => NewChi, <<"last-drip">> => Last + 1 }.
 
-rate(_S = #{ <<"last-drip">> := _ }) ->
+reward_per_unit(_S = #{ <<"last-drip">> := _, <<"total">> := Total }) ->
     % Statically mint one new token proportionate to ownership, for now.
-    0.1.
+    1 * (1 / Total).
 
 %%% Helpers.
 
@@ -82,10 +81,9 @@ balance(deposit, Addr, #{ <<"balances">> := Balances }) ->
     end;
 balance(reward, Addr, #{ <<"balances">> := Balances, <<"chi">> := ChiN }) ->
     case maps:find(Addr, Balances) of
-        {ok, Entry = #{ <<"deposit">> := Deposit, <<"chi0">> := Chi0 }} ->
-            % Calculate the acrued yield and add it to the existing reward balance.
-            Yield = unpie(pie(Deposit, Chi0), ChiN) - Deposit,
-            Yield + maps:get(<<"reward">>, Entry, 0);
+        {ok, #{ <<"deposit">> := Deposit, <<"chi0">> := Chi0, <<"reward">> := Existing }} ->
+            Yield = (ChiN - Chi0) * Deposit,
+            Existing + Yield;
         error -> 0
     end.
 
@@ -120,5 +118,6 @@ modify(Addr, Amount, S) ->
         maps:get(<<"balances">>, NewS, #{})
     ),
     NewS#{
-        <<"balances">> => NewBalances
+        <<"balances">> => NewBalances,
+        <<"total">> => maps:get(<<"total">>, NewS, 0) + Amount
     }.
