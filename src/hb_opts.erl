2096882047c99449aef09036381f938a49e14328
Ayush Agrawal <ayush.aa347@gmail.com>
2025-11-05 18:32:33 +0530

    chore: token device added to opts

--- src/hb_opts.erl
+++ src/hb_opts.erl
@@ -180,6 +180,7 @@ default_message() ->
             #{<<"name">> => <<"stack@1.0">>, <<"module">> => dev_stack},
             #{<<"name">> => <<"structured@1.0">>, <<"module">> => dev_codec_structured},
             #{<<"name">> => <<"test-device@1.0">>, <<"module">> => dev_test},
+            #{<<"name">> => <<"token@1.0">>, <<"module">> => dev_token},
             #{<<"name">> => <<"trie@1.0">>, <<"module">> => dev_trie},
             #{<<"name">> => <<"volume@1.0">>, <<"module">> => dev_volume},
             #{<<"name">> => <<"secret@1.0">>, <<"module">> => dev_secret},
