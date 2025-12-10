d9bee61213c5bef6e110c796e89d273eb42b0904
Sam Williams <sam@arweave.org>
2025-12-09 19:24:37 -0500

    wip: MVP AO-Core property-based testing framework. No docs yet.

--- src/dev_trie_prop.erl
+++ src/dev_trie_prop.erl
@@ -3,12 +3,32 @@
 -include_lib("eunit/include/eunit.hrl").
 
 model_test() ->
-    hb_prop:state_machine_with_model(
-        #{ <<"device">> => <<"trie@1.0">> },
-        #{ <<"device">> => <<"message@1.0">> },
-        requests(),
-        properties()
-    ).
+    Res =
+        hb_prop:state_machine(
+            #{
+                state => #{ <<"device">> => <<"trie@1.0">>, <<"a">> => 1 },
+                model_state => #{ <<"device">> => <<"message@1.0">>, <<"a">> => 1 },
+                request_gen => requests(),
+                properties => properties(),
+                next => fun next/4,
+                runs => 10,
+                seq_len => 10,
+                opts => #{}
+            }
+        ),
+    case Res of
+        {failure, InitialState, ResSequence} ->
+            ?event(
+                properties,
+                {failure,
+                    {initial_state, InitialState},
+                    {sequence, ResSequence}
+                },
+                #{ debug_print_truncate => infinity }
+            ),
+            error(failure);
+        ok -> ok
+    end.
 
 requests() ->
     [
@@ -16,14 +36,14 @@ requests() ->
     ||
         Action <- [set, get]
     ].
-request(set, S, Opts) ->
+request(set, _S, _Opts) ->
     #{
         <<"path">> => <<"set">>,
         hb_prop:key() => hb_prop:any()
     };
 request(get, S, Opts) ->
     #{
-        <<"path">> => hb_prop:pick(hb_ao:keys(S, Opts))
+        <<"path">> => hb_prop:pick(hb_ao:keys(S, Opts) -- [<<"device">>])
     }.
 
 properties() ->
@@ -34,13 +54,34 @@ properties() ->
     ].
 
 verify_set(_O1, _O2, Req = #{ <<"path">> := <<"set">> }, New1, New2, Opts) ->
-    {ok, Key} = hb_maps:find(<<"key">>, Req, Opts),
+    ?event(
+        {retrievability,
+            {request, Req},
+            {new_state, New1},
+            {new_model_state, New2}
+        }
+    ),
+    [Key] = hb_maps:keys(Req, Opts) -- [<<"path">>],
     hb_ao:resolve(New1, Key, Opts) == hb_ao:resolve(New2, Key, Opts).
 
 verify_size(Old, #{ <<"path">> := <<"set">> }, New, Opts) ->
+    ?event(
+        {size,
+            {old_state, Old},
+            {new_state, New}
+        }
+    ),
     NumNewKeys = length(hb_ao:keys(New, Opts)),
     NumOldKeys = length(hb_ao:keys(Old, Opts)),
     (NumNewKeys == NumOldKeys) orelse (NumNewKeys == NumOldKeys + 1).
 
 verify_commitments(_, #{ <<"path">> := <<"set">> }, New, Opts) ->
+    ?event(
+        {commitments,
+            {new_state, New}
+        }
+    ),
     hb_message:verify(New, all, Opts).
+
+next(_OldS, #{ <<"path">> := <<"set">> }, NewS, _Opts) -> NewS;
+next(OldS, _, _NewS, _Opts) -> OldS.
--- src/hb_prop.erl
+++ src/hb_prop.erl
@@ -1,5 +1,5 @@
 -module(hb_prop).
--export([state_machine/3, state_machine/4, state_machine_with_model/4]).
+-export([state_machine/1]).
 -export([any/0, any/1, pick/1]).
 -export([int/0, int/1, int/2, float/0, float/1]).
 -export([string/0, string/1, string/4, key/0, key/1]).
@@ -10,65 +10,65 @@
 -define(DEFAULT_PROPERTY_RUNS, 10).
 -define(DEFAULT_PROPERTY_SEQ_LEN, 10).
 
-state_machine(InitialState, RequestGen, Properties) ->
-    state_machine(InitialState, RequestGen, Properties, #{}).
-state_machine(InitialState, RequestGen, Properties, Opts) ->
-    Runs = hb_opts:get(property_runs, ?DEFAULT_PROPERTY_RUNS, Opts),
-    SeqLen = hb_opts:get(property_seq_len, ?DEFAULT_PROPERTY_SEQ_LEN, Opts),
-    state_machine(
-        #{
-            runs => Runs,
-            seq_len => SeqLen,
-            state => InitialState,
-            model_state => undefined,
-            request_gen => RequestGen,
-            properties => Properties,
-            opts => Opts
+state_machine(Spec = #{ request_gen := _ }) ->
+    run_state_machines(
+        Spec#{
+            state => hb_opts:get(state, #{}, Spec),
+            model_state => hb_opts:get(model_state, undefined, Spec),
+            runs => hb_opts:get(property_runs, ?DEFAULT_PROPERTY_RUNS, Spec),
+            seq_len => hb_opts:get(property_seq_len, ?DEFAULT_PROPERTY_SEQ_LEN, Spec),
+            properties => hb_opts:get(properties, [], Spec),
+            next => hb_opts:get(next, undefined, Spec),
+            opts => hb_opts:get(opts, #{}, Spec)
         }
-    ).
+    );
+state_machine(_Spec) ->
+    throw({invalid_spec, missing_request_generator}).
 
-state_machine_with_model(InitState, ModelInitState, RequestGen, Properties) ->
-    state_machine_with_model(InitState, ModelInitState, RequestGen, Properties, #{}).
-state_machine_with_model(InitState, ModelInitState, RequestGen, Properties, Opts) ->
-    Runs = hb_opts:get(property_runs, ?DEFAULT_PROPERTY_RUNS, Opts),
-    SeqLen = hb_opts:get(property_seq_len, ?DEFAULT_PROPERTY_SEQ_LEN, Opts),
-    state_machine(
-        #{
-            runs => Runs,
-            seq_len => SeqLen,
-            state => InitState,
-            model_state => ModelInitState,
-            request_gen => RequestGen,
-            properties => Properties,
-            opts => Opts
-        }
-    ).
-
-state_machine(#{ runs := 0 }) ->
+run_state_machines(#{ runs := 0 }) ->
     ok;
-state_machine(Spec = #{ runs := Runs }) ->
-    case state_machine_loop(Spec) of
-        ok -> state_machine(Spec#{ runs => Runs - 1 });
-        {error, Reason} -> {error, Reason}
+run_state_machines(Spec = #{ runs := Runs }) ->
+    InitialState = generate_initial_state(Spec),
+    ResSequence = state_machine_loop(Spec#{ state => InitialState }),
+    case lists:last(ResSequence) of
+        {error, _Type, _Reason} ->
+            {failure, InitialState, ResSequence};
+        ok ->
+            ?event(properties,
+                {successful_sequence, [InitialState | ResSequence]}
+            ),
+            run_state_machines(Spec#{ runs => Runs - 1 })
     end.
 
 state_machine_loop(#{ seq_len := 0 }) -> ok;
 state_machine_loop(Spec) ->
-    case execute_request(Spec, Req = generate_request(Spec)) of
-        {error, Reason} ->
-            {error, Reason};
+    Req = generate_request(Spec),
+    ?event(
+        {evaluating_request,
+            {request, Req},
+            {specification, Spec}
+        }
+    ),
+    case execute_request(Spec, Req) of
+        {error, Type, Reason} ->
+            [Req, {error, Type, Reason}];
         Result ->
             case enforce_properties(Spec, Req, Result) of
                 ok ->
                     [
                         Req
                     |
-                        state_machine_loop(apply_result(Spec, Result))
+                        state_machine_loop(apply_result(Spec, Req, Result))
                     ];
-                {error, Reason} ->
-                    [Req, {error, Reason}]
+                {error, Type, Reason} ->
+                    [Req, {error, Type, Reason}]
             end
     end.
+
+generate_initial_state(#{ state := Gen, opts := Opts }) when is_function(Gen) ->
+    Gen(Opts);
+generate_initial_state(#{ state := InitialState }) ->
+    InitialState.
 
 generate_request(#{ request_gen := RequestGen, state := State, opts := Opts }) ->
     generate_request(State, RequestGen, Opts).
@@ -84,9 +84,9 @@ execute_request(#{ model_state := ModelState, state := State, opts := Opts }, Re
         {{ok, NewState}, {ok, NewModelState}} ->
             {ok, NewState, NewModelState};
         {{error, Reason}, _} ->
-            {primary_error, Reason};
+            {error, request_error, Reason};
       {_, {error, Reason}} ->
-            {model_error, Reason}
+            {error, model_request_error, Reason}
     end.
 
 enforce_properties(Spec = #{ properties := Properties }, Req, Result) ->
@@ -94,17 +94,34 @@ enforce_properties(Spec = #{ properties := Properties }, Req, Result) ->
 enforce_properties([], _Req, _Result, _Spec) -> ok;
 enforce_properties([Property | Properties], Req, Result, Spec) ->
     case {enforce_property(Property, Req, Result, Spec), Result} of
-        {skip, {ok, NewState, _NewModelState}} ->
+        {downgrade, {ok, NewState, _NewModelState}} ->
+            ?event(
+                {falling_back_to_primary_state_enforcement,
+                    {property, Property},
+                    {request, Req},
+                    {new_state, NewState}
+                }
+            ),
             case enforce_property(Property, Req, {ok, NewState}, Spec) of
-                ok ->
+                X when X =:= ok orelse X =:= skip ->
+                    ?event(
+                        {downgraded_property_enforced,
+                            {status, X},
+                            {property, Property}
+                        }
+                    ),
                     enforce_properties(Properties, Req, Result, Spec);
-                {error, Reason} ->
-                    {error, Reason}
+                {error, Reason} -> {error, {property_error, Property}, Reason}
             end;
-        {ok, _} ->
+        {X, _} when X =:= ok orelse X =:= skip ->
+            ?event(
+                {property_enforced,
+                    {status, X},
+                    {property, Property}
+                }
+            ),
             enforce_properties(Properties, Req, Result, Spec);
-        {error, Reason} ->
-            {error, Reason}
+        {error, Reason} -> {error, {property_error, Property}, Reason}
     end.
 
 enforce_property(
@@ -121,6 +138,7 @@ enforce_property(
         false -> {error, Property};
         skip -> skip
     catch
+        error:{badarity, _} -> downgrade;
         error:function_clause -> skip
     end;
 enforce_property(
@@ -136,18 +154,43 @@ enforce_property(
         false -> {error, Property};
         skip -> skip
     catch
+        error:{badarity, _} -> skip;
         error:function_clause -> skip
     end.
 
-apply_result(Spec = #{ model_state := undefined }, {ok, NewState}) ->
+apply_result(Spec = #{ next := undefined, model_state := undefined }, _, {ok, NewState}) ->
     Spec#{ state => NewState };
-apply_result(Spec, {{ok, NewState}, {ok, NewModelState}}) ->
-    Spec#{ model_state => NewModelState, state => NewState }.
+apply_result(Spec = #{ next := undefined }, _, {ok, NewState, NewModelState}) ->
+    Spec#{ model_state => NewModelState, state => NewState };
+apply_result(
+        Spec = #{
+            next := Next,
+            state := OldState,
+            model_state := OldModelState,
+            opts := Opts
+        },
+        Req,
+        {ok, NewState, NewModelState}) ->
+    Spec#{
+        state => Next(OldState, Req, NewState, Opts),
+        model_state => Next(OldModelState, Req, NewModelState, Opts)
+    };
+apply_result(
+        Spec = #{
+            next := Next,
+            state := OldState,
+            opts := Opts
+        },
+        Req,
+        {ok, NewState}) ->
+    Spec#{
+        state => Next(OldState, Req, NewState, Opts)
+    }.
 
 %%% Type generators.
 
 %% Size constants.
--define(BUILTIN_TYPES, [int, float, atom, string, key]).
+-define(BUILTIN_TYPES, [int, float, string, key]).
 -define(INT_MAX, 1 bsl 32).
 -define(INT_TINY_MAX, 32).
 -define(SMALL_INT_MAX, 256).
@@ -185,7 +228,7 @@ float(big) -> rand:uniform_real() * (2 * ?BIG_INT_MAX);
 float(Max) -> rand:uniform_real() * (2 * Max).
 
 string() -> string(?STRING_MAX_LENGTH).
-string(MaxLen) -> string(MaxLen, 32, 126, [$/]).
+string(MaxLen) -> string(MaxLen, 65, 90, [$/]).
 string(MaxLen, MinChar, MaxChar, Forbidden) ->
     <<
         <<(pick(MinChar, MaxChar, Forbidden)):8>>
