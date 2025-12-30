2c54a049976ac37bfb6fa0db90f0179e2eff5743
Noah Levenson <noahlevenson@gmail.com>
2025-12-30 13:49:36 -0800

    wip: add withdraw generator and properties, refactor

--- src/dev_pot_props.erl
+++ src/dev_pot_props.erl
@@ -12,7 +12,10 @@ simulation_test() ->
             requests => generate_request(),
             states => fun generate_initial_state/1,
             properties => [
-                fun verify_deposit/4
+                fun verify_deposit_quantity/4,
+                fun verify_withdraw_quantity/4,
+                fun verify_withdraw_liquidation/4,
+                fun verify_twu/4
             ],
             runs => 3,
             length => 4,
@@ -83,8 +86,8 @@ generate_initial_state(Opts) ->
 
 generate_request() ->
     [
-        fun deposit_generator/2
-        % fun withdraw_generator/0,
+        fun deposit_generator/2,
+        fun withdraw_generator/2
         % fun delegate_generator/0,
         % fun undelegate_generator/0
     ].
@@ -103,13 +106,43 @@ deposit_generator(_State, Opts) ->
                     ),
                 <<"quantity">> => hb_invariant:int(1, 1_000_000),
                 <<"resource">> => hb_invariant:pick(hb_maps:get(resources, Opts)),
-                <<"from">> => <<"foo">> % TODO: What should this value be?
+                <<"from">> => <<"foo">>, % TODO: What should this value be?
+                <<"t">> => hb_invariant:int(100000)
             }
         },
         Opts#{ priv_wallet => Wallet }
     ).
 
-verify_deposit(OldState, Req, NewState, Opts) ->
+withdraw_generator(State, Opts) ->
+    % TODO: in theory, over the course of a generated scenario, there might exist
+    % a deposit with a quantity of 0. We shouldn't pick those.
+    Addrs = hb_maps:keys(hb_maps:get(<<"users">>, State)),
+    UserAddr = hb_invariant:pick(Addrs),
+    Deposits = hb_ao:get(
+        <<"/users/", UserAddr/binary, "/deposits">>,
+        State,
+        #{},
+        Opts
+    ),
+    ResourceIDs = hb_maps:keys(hb_private:reset(Deposits)),
+    UserResourceID = hb_invariant:pick(ResourceIDs),
+    CurrentQty = hb_maps:get(UserResourceID, Deposits),
+    Wallet = hb_maps:get(priv_wallet, hb_maps:get(UserAddr, hb_maps:get(identities, Opts))),
+    hb_message:commit(
+        #{
+            <<"path">> => <<"withdraw">>,
+            <<"body">> => #{
+                <<"address">> => UserAddr,
+                <<"quantity">> => hb_invariant:int(1, CurrentQty),
+                <<"resource">> => UserResourceID,
+                <<"from">> => <<"foo">>, % TODO: What should this value be?
+                <<"t">> => hb_invariant:int(100000)
+            }
+        },
+        Opts#{ priv_wallet => Wallet }
+    ).
+
+verify_deposit_quantity(OldState, Req = #{ <<"path">> := <<"deposit">> }, NewState, Opts) ->
     UnwrappedReq = hb_maps:get(<<"body">>, Req),
     Addr = hb_maps:get(<<"address">>, UnwrappedReq),
     ResourceID = hb_maps:get(<<"resource">>, UnwrappedReq),
@@ -126,7 +159,6 @@ verify_deposit(OldState, Req, NewState, Opts) ->
         0,
         Opts
     ),
-    % Simple verification of new deposit quantity
     NewDeposit =:= OldDeposit + Quantity orelse
     {error,
         {bad_deposit_math,
@@ -134,7 +166,111 @@ verify_deposit(OldState, Req, NewState, Opts) ->
             {new_deposit, NewDeposit},
             {qty, Quantity}
         }
-    },
+    }.
+
+verify_withdraw_quantity(OldState, Req = #{ <<"path">> := <<"withdraw">> }, NewState, Opts) ->
+    UnwrappedReq = hb_maps:get(<<"body">>, Req),
+    Quantity = hb_maps:get(<<"quantity">>, UnwrappedReq),
+    Addr = hb_maps:get(<<"address">>, UnwrappedReq),
+    ResourceID = hb_maps:get(<<"resource">>, UnwrappedReq),
+    OldDeposit = hb_ao:get(
+        <<"/resources/", ResourceID/binary, "/deposits/", Addr/binary, "/quantity">>,
+        OldState,
+        0,
+        Opts
+    ),
+    NewDeposit = hb_ao:get(
+        <<"/resources/", ResourceID/binary, "/deposits/", Addr/binary, "/quantity">>,
+        NewState,
+        0,
+        Opts
+    ),
+    case OldDeposit >= Quantity of
+        true ->
+            % No liquidation required
+            NewDeposit =:= OldDeposit - Quantity orelse
+            {error,
+                {bad_withdraw_math_no_liquidation,
+                    {old_deposit, OldDeposit},
+                    {new_deposit, NewDeposit},
+                    {qty, Quantity}
+                }
+            };
+        false ->
+            % Liquidation required
+            NewDeposit =:= 0 orelse
+            {error,
+                {bad_withdraw_math_with_liquidation,
+                    {old_deposit, OldDeposit},
+                    {new_deposit, NewDeposit},
+                    {qty, Quantity}
+                }
+            }
+    end.
+
+verify_withdraw_liquidation(OldState, Req = #{ <<"path">> := <<"withdraw">> }, NewState, Opts) ->
+    UnwrappedReq = hb_maps:get(<<"body">>, Req),
+    Quantity = hb_maps:get(<<"quantity">>, UnwrappedReq),
+    Addr = hb_maps:get(<<"address">>, UnwrappedReq),
+    ResourceID = hb_maps:get(<<"resource">>, UnwrappedReq),
+    OldDeposit = hb_ao:get(
+        <<"/resources/", ResourceID/binary, "/deposits/", Addr/binary, "/quantity">>,
+        OldState,
+        0,
+        Opts
+    ),
+    OldDelegations = hb_ao:get(
+        <<
+            "/resources/",
+            ResourceID/binary,
+            "/deposits/",
+            Addr/binary,
+            "/delegations"
+        >>,
+        OldState,
+        #{},
+        Opts
+    ),
+    NewDelegations = hb_ao:get(
+        <<
+            "/resources/",
+            ResourceID/binary,
+            "/deposits/",
+            Addr/binary,
+            "/delegations"
+        >>,
+        NewState,
+        #{},
+        Opts
+    ),
+    case OldDeposit >= Quantity of
+        true ->
+            % No liquidation required
+            OldDelegations =:= NewDelegations orelse
+            {error,
+                {delegation_table_mutated,
+                    {old_table, OldDelegations},
+                    {new_table, NewDelegations}
+                }
+            };
+        false ->
+            % Liquidation required
+            SumOldDelegations = lists:sum(hb_maps:values(OldDelegations)),
+            SumNewDelegations = lists:sum(hb_maps:values(NewDelegations)),
+            SumOldDelegations - SumNewDelegations =:=
+                Quantity - OldDeposit orelse
+                    {error,
+                        {incoherent_liquidation,
+                            {old_table, OldDelegations},
+                            {new_table, NewDelegations}
+                        }
+                    }
+    end.
+
+verify_twu(OldState, Req, NewState, Opts) ->
+    UnwrappedReq = hb_maps:get(<<"body">>, Req),
+    Quantity = hb_maps:get(<<"quantity">>, UnwrappedReq),
+    ResourceID = hb_maps:get(<<"resource">>, UnwrappedReq),
     Weight = hb_ao:get(
         <<"/resources/", ResourceID/binary, "/weight">>,
         NewState,
@@ -143,28 +279,21 @@ verify_deposit(OldState, Req, NewState, Opts) ->
     ),
     OldTWU = hb_maps:get(<<"total-weighted-units">>, OldState),
     NewTWU = hb_maps:get(<<"total-weighted-units">>, NewState),
-    % Verify new total weighted units
-    NewTWU =:= OldTWU + (Quantity * Weight) orelse
+    Path = hb_maps:get(<<"path">>, Req),
+    Res =
+        case Path of
+            <<"deposit">> ->
+                NewTWU =:= OldTWU + (Quantity * Weight);
+            <<"withdraw">> ->
+                NewTWU =:= OldTWU - (Quantity * Weight)
+        end,
+    Res orelse
     {error,
         {bad_total_weighted_units,
             {old_twu, OldTWU},
             {new_twu, NewTWU},
             {weight, Weight},
             {qty, Quantity}
-        }
-    },
-    InvertedQty = hb_ao:get(
-        <<"/users/", Addr/binary, "/deposits/", ResourceID/binary>>,
-        NewState,
-        0,
-        Opts
-    ),
-    % Simple verification of inverted index
-    InvertedQty =:= NewDeposit orelse
-    {error,
-        {bad_inverted_index,
-            {inverted_deposit, InvertedQty},
-            {new_deposit, NewDeposit}
         }
     }.
 
