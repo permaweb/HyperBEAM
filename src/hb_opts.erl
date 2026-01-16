dab07046a0007161c9134d43f47c90cdb1e58e06
James Piechota <piechota@gmail.com>
2026-01-16 16:49:59 -0500

    wip: implement ~arweave@2.9-pre/chunk

--- src/hb_opts.erl
+++ src/hb_opts.erl
@@ -335,7 +335,7 @@ default_message() ->
                 <<"node">> =>
                     #{
                         <<"match">> => <<"^/arweave">>,
-                        <<"with">> => <<"https://arweave.net">>,
+                        <<"with">> => <<"http://tip-3.arweave.xyz:1984">>,
                         <<"opts">> => #{ http_client => httpc, protocol => http2 }
                     }
             },
