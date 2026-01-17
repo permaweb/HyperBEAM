6dcd3ef0ad8b377579c0a7a900bb5c0f0d99399e
James Piechota <piechota@gmail.com>
2026-01-16 21:21:37 -0500

    test: make dev_arweave tests more reliable

--- src/hb_opts.erl
+++ src/hb_opts.erl
@@ -294,7 +294,7 @@ default_message() ->
                 <<"node">> =>
                     #{
                         <<"match">> => <<"^/arweave">>,
-                        <<"with">> => <<"http://tip-3.arweave.xyz:1984">>,
+                        <<"with">> => <<"https://arweave.net">>,
                         <<"opts">> => #{ http_client => httpc, protocol => http2 }
                     }
             },
