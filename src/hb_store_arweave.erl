25d1771c53e1cd576cbec14a84307712ccda504c
James Piechota <piechota@gmail.com>
2026-01-21 17:26:49 -0500

    wip: working on getting a single data item from a bundle to load

--- src/hb_store_arweave.erl
+++ src/hb_store_arweave.erl
@@ -10,33 +10,47 @@
 
 -define(ARWEAVE_INDEX_PATH, <<"~arweave@2.9-pre/offset">>).
 
-read(StoreOpts = #{ <<"arweave-index-store">> := IndexStore }, ID) ->
+read(StoreOpts = #{ <<"index-store">> := IndexStore }, ID) ->
     case hb_store:read(IndexStore, path(ID)) of
         {ok, Binary} ->
-            [IsTX, Offset, Length] = binary:split(Binary, <<":">>, [global]),
+            % EndOffset and Size is recorded (rather than StartOffset and
+            % Length) to preserve consistency with the Arweave API's
+            % `/tx/<hash>/offset` endpoint.
+            [IsTX, EndOffset, Size] = binary:split(Binary, <<":">>, [global]),
             ?event(
                 debug_test,
-                {reading_offset, {is_tx, IsTX}, {offset, Offset}, {length, Length}}
+                {reading_offset, {is_tx, IsTX},
+                {end_offset, EndOffset}, {size, Size}}
             ),
             case hb_util:bool(IsTX) of
                 true ->
-                    load_bundle(ID, Offset, Length, StoreOpts);
+                    load_bundle(ID,
+                        hb_util:int(EndOffset), hb_util:int(Size), StoreOpts);
                 false ->
-                    load_item(Offset, Length, StoreOpts)
+                    load_item(
+                        hb_util:int(EndOffset), hb_util:int(Size), StoreOpts)
             end;
         {error, not_found} ->
             {error, not_found}
     end.
 
-load_item(Offset, Length, Opts) ->
-    case read_chunks(Offset, Length, Opts) of
+load_item(EndOffset, Size, Opts) ->
+    case read_chunks(EndOffset, Size, Opts) of
         {ok, SerializedItem} ->
-            to_message(ar_bundles:deserialize(SerializedItem), Opts);
+            {
+                ok,
+                hb_message:convert(
+                    ar_bundles:deserialize(SerializedItem),
+                    <<"structured@1.0">>,
+                    <<"ans104@1.0">>,
+                    Opts
+                )
+            };
         {error, Reason} ->
             {error, Reason}
     end.
 
-load_bundle(ID, Offset, Length, Opts) ->
+load_bundle(ID, EndOffset, Size, Opts) ->
     {ok, StructuredTXHeader} = hb_ao:resolve(
         #{ <<"device">> => <<"arweave@2.9-pre">> },
         #{ <<"path">> => <<"tx">>, <<"tx">> => ID, <<"exclude-data">> => true },
@@ -47,50 +61,49 @@ load_bundle(ID, Offset, Length, Opts) ->
         <<"tx@1.0">>,
         <<"structured@1.0">>,
         Opts),
-    case read_chunks(Offset, Length, Opts) of
+    case read_chunks(EndOffset, Size, Opts) of
         {ok, SerializedItem} ->
-            to_message(
-                ar_bundles:deserialize(TXHeader#tx{ data = SerializedItem }),
-                Opts);
+            {
+                ok,
+                hb_message:convert(
+                    TXHeader#tx{ data = SerializedItem },
+                    <<"structured@1.0">>,
+                    <<"tx@1.0">>,
+                    Opts
+                )
+            };
         {error, Reason} ->
             {error, Reason}
     end.
 
-read_chunks(Offset, Length, Opts) ->
+read_chunks(EndOffset, Size, Opts) ->
+    StartOffset = EndOffset - Size + 1,
     hb_ao:resolve(
         #{ <<"device">> => <<"arweave@2.9-pre">> },
         #{
             <<"path">> => <<"chunk">>,
-            <<"offset">> => Offset,
-            <<"length">> => Length
+            <<"offset">> => StartOffset,
+            <<"length">> => Size
         },
         Opts
     ).
 
-to_message(TX, Opts) ->
-    {
-        ok,
-        hb_message:convert(
-            TX,
-            <<"structured@1.0">>,
-            <<"tx@1.0">>,
-            Opts
-        )
-    }.
 
-write_offset(Store, ID, IsTX, Offset, Length) ->
+%% @doc When recording an item or bundle's offset we use its EndOffset and
+%% Size in rather than StartOffset and Length - this is for consistency with
+%% the Arweave API's `/tx/<hash>/offset` endpoint which returns a global
+%% end offset and size.
+write_offset(#{ <<"index-store">> := IndexStore }, ID, IsTX, EndOffset, Size) ->
     IsTxInt = hb_util:bool_int(IsTX),
-    hb_store:write(
-        Store,
-        path(ID), 
-        <<
-            (hb_util:bin(IsTxInt))/binary,
-            ":",
-            (hb_util:bin(Offset))/binary,
-            ":",
-            (hb_util:bin(Length))/binary
-        >>
-    ).
+    Value = <<
+        (hb_util:bin(IsTxInt))/binary,
+        ":",
+        (hb_util:bin(EndOffset))/binary,
+        ":",
+        (hb_util:bin(Size))/binary
+    >>,
+    ?event(debug_test, {value, Value}),
+    hb_store:write(IndexStore, path(ID), Value).
 
 path(ID) ->
     <<
@@ -106,12 +119,12 @@ write_read_tx_test() ->
     Store = [hb_test_utils:test_store()],
     ?event(debug_test, {store, Store}),
     Opts = #{ 
-        <<"arweave-index-store">> => Store 
+        <<"index-store">> => Store 
     },
     ID = <<"bndIwac23-s0K11TLC1N7z472sLGAkiOdhds87ZywoE">>,
-    Offset = 363524457275639,
-    Length = 8387,
-    ok = write_offset(Store, ID, true, Offset, Length),
+    EndOffset = 363524457284025,
+    Size = 8387,
+    ok = write_offset(Opts, ID, true, EndOffset, Size),
     {ok, Bundle} = read(Opts, ID),
     ?assert(hb_message:verify(Bundle, all, #{})),
     {ok, Child} =
@@ -134,3 +147,5 @@ write_read_tx_test() ->
     },
     ?assert(hb_message:match(ExpectedChild, Child, only_present)),
     ok.
+
+%% XXX TODO: write/read for data item
