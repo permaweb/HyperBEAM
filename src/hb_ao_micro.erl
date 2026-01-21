62b7001e10fe1d989e859e74dec702dd3a6047f0
Sam Williams <sam@arweave.org>
2026-01-20 23:50:49 -0500

    feat: a simple AO-Core 1.5 resolver spec implementation.

--- src/hb_ao_micro.erl
+++ src/hb_ao_micro.erl
@@ -1,70 +1,185 @@
 %%% @doc A minimal implementation of the AO-Core 1.5 resolution system.
 -module(hb_ao_micro).
--export([resolve/2, resolve/3]).
+-export([get/3, resolve/2, resolve/3]).
+-include("include/hb.hrl").
 
-resolve(Path, Opts) ->
-    [Base|Reqs] = hb_path:term_to_path_parts(Path, Opts),
-    lists:foldl(
-        fun(Req, XBase) -> resolve(XBase, Req, Opts) end,
-        Base,
-        Reqs
-    ).
+%% @doc Shortcut to resolve a key and return the result, unwrapping `resolve's
+%% `{ok, ...}' return.
+get(Key, Msg, Opts) ->
+    hb_util:ok(resolve(Msg, Key, Opts)).
+
+%% @doc Take a path or sequence of messages and resolve them sequentially. The
+%% result of the resolution of the first element of the path/list against the 
+%% second is used as the base for the third element, and so on. The resulting
+%% message after the final pair has been evaluated is returned.
+-spec resolve(binary() | [binary() | map()], map()) -> {ok, any()}.
+resolve(Path, Opts) when is_binary(Path) ->
+    resolve(hb_path:term_to_path_parts(Path, Opts), Opts);
+resolve([Base, Req|Rest], Opts) ->
+    case resolve(Base, Req, Opts) of
+        {ok, Result} when length(Rest) == 0 -> {ok, Result};
+        {ok, Result} -> resolve([Result|Rest], Opts);
+        {error, Reason} -> {error, Reason}
+    end.
 
 %% @doc Resolve a hashpath via its `Base` and `Req` (`Prefix/Suffix`) components.
-%% Stages:
-%% 1. Normalize.
-%% 2. Try to read the key directly. Return if found.
-%% 3. Try to read the `device' of the `Base'. Return `Not Found' on absence.
-%%    Additionally, extract the `path' from the `Req'.
-%% 4. Read `Device/Req' from device. Return `Not Found' on absence.
-%% 5. Vary the `Base' and `Req' by the `device' and `path'.
-%% 6. Try to read `VariedBase/VariedReq' from the cache. Return if found.
-%% 7. Execute the device function.
-%% 8. Write the result to the cache. Link `VariedBase/VariedReq' to `Result'.
-%% 9. Replace `... : base` in the result with `... : OriginalBaseID' if present.
-%% 10. Return the result.
-resolve(Base, Req, Opts) -> stage(1, Base, Req, Opts).
-stage(1, Base, Req, Opts) when not is_binary(Base) ->
+%% Each of the stages is a separate function with its own arguments, building up
+%% the state of the computation as it progresses.
+-spec resolve(binary() | map(), binary() | map(), map()) -> {ok, any()}.
+resolve(Base, Req, Opts) -> stage_1(Base, Req, Opts).
+
+%% @doc Stage 1: Normalize the `Base' and `Req' components.
+%% If either of the components is not a binary, we write them to the cache and 
+%% use the resulting IDs as the `Base' and `Req' for the next stage.
+stage_1(Base, Req, Opts) when not is_binary(Base) ->
+    ?event(ao_core, {normalize_offloading_base, Base}, Opts),
     {ok, BaseID} = hb_cache:write(Base, Opts),
-    stage(1, BaseID, Req, Opts);
-stage(1, BaseID, Req, Opts) when not is_binary(Req) ->
+    stage_1(BaseID, Req, Opts);
+stage_1(BaseID, Req, Opts) when not is_binary(Req) ->
+    ?event(ao_core, {normalize_offloading_req, Req}, Opts),
     {ok, ReqID} = hb_cache:write(Req, Opts),
-    stage(1, BaseID, ReqID, Opts);
-stage(1, BaseID, ReqID, Opts) ->
-    stage(2, BaseID, ReqID, Opts);
-stage(2, BaseID, ReqID, Opts) ->
-    case hb_cache:read(<<BaseID/binary, "/", ReqID/binary>>, Opts) of
-        {ok, Msg} -> {ok, Msg};
-        not_found -> stage(3, BaseID, ReqID, Opts)
-    end;
-stage(3, BaseID, ReqID, Opts) ->
+    stage_1(BaseID, ReqID, Opts);
+stage_1(BaseID, ReqID, Opts) ->
+    stage_2(BaseID, ReqID, Opts).
+
+%% @doc Stage 2: Try to read the key directly. Return if found.
+%% If not found, we move on to the next stage.
+stage_2(BaseID, ReqID, Opts) ->
+    case hb_cache:read(HP = <<BaseID/binary, "/", ReqID/binary>>, Opts) of
+        {ok, Result} ->
+            ?event(ao_core, {cache_hit, {path, HP}, Result}, Opts),
+            {ok, Result};
+        not_found ->
+            stage_3(BaseID, ReqID, Opts)
+    end.
+
+%% @doc Stage 3: Try to read the `device' of the `BaseID' and the `path' of the
+%% `ReqID'. The default device is `message@1.0', and absence of a `path' results
+%% in a `throw'.
+stage_3(BaseID, ReqID, Opts) ->
     DeviceID =
         case hb_cache:read(<<BaseID/binary, "/device">>, Opts) of
-            {ok, DeviceID} -> DeviceID;
+            {ok, Device} -> Device;
             not_found -> <<"message@1.0">>
         end,
-    Path =
-        case hb_cache:read(<<ReqID/binary, "/path">>, Opts) of
-            {ok, Path} -> Path;
-            not_found -> <<"set">>
-        end,
-    stage(4, BaseID, ReqID, DeviceID, Path, Opts);
-stage(4, BaseID, ReqID, Device, Path, Opts) ->
-    case hb_cache:read(<<Device/binary, "/", ReqID/binary>>, Opts) of
-        {ok, Func} -> stage(5, BaseID, ReqID, Device, Path, Func, Opts);
-        not_found -> {error, not_found}
-    end;
-stage(5, BaseID, ReqID, Device, Path, Func, Opts) ->
-    
-    {ok, VariedBase, VariedReq} = hb_types:vary(Device, Path, BaseID, ReqID, Opts),
-    stage(6, BaseID, ReqID, Device, VariedBase, VariedReq, Opts).
-stage(6, BaseID, ReqID, Device, VariedBase, VariedReq, Opts) ->
-    case hb_cache:read(<<VariedBase/binary, "/", VariedReq/binary>>, Opts) of
-        {ok, Msg} -> {ok, Msg};
-        not_found -> stage(7, BaseID, ReqID, Device, VariedBase, VariedReq, Opts)
-    end;
-stage(7, BaseID, ReqID, Device, VariedBase, VariedReq, Opts) ->
-    Msg = hb_device:apply(Device, VariedBase, VariedReq, Opts),
-    hb_cache:write(<<VariedBase/binary, "/", VariedReq/binary>>, Msg, Opts),
-    hb_cache:link(<<BaseID/binary, "/", ReqID/binary>>, <<VariedBase/binary, "/", VariedReq/binary>>, Opts),
-    {ok, Msg}.
+    case not ?IS_ID(ReqID) of
+        true -> stage_4(BaseID, ReqID, DeviceID, ReqID, Opts);
+        false ->
+            case hb_cache:read(<<ReqID/binary, "/path">>, Opts) of
+                {ok, Key} -> stage_4(BaseID, ReqID, DeviceID, Key, Opts);
+                not_found -> throw({no_path_in_request, {base, BaseID}, {req, ReqID}})
+            end
+    end.
+
+%% @doc Stage 4: Read the device and key from the cache. We expect to find a
+%% `resolver' function and a `vary' function in return.
+stage_4(BaseID, ReqID, DeviceID, Key, Opts) ->
+    case hb_cache:read(<<DeviceID/binary, "/", Key/binary>>, Opts) of
+        {ok, #{ <<"resolver">> := Func, <<"vary">> := Vary }} ->
+            ?event(ao_core,
+                {found_resolver_and_vary,
+                    {device, DeviceID},
+                    {key, Key},
+                    {resolver, Func},
+                    {vary, Vary}
+                },
+                Opts
+            ),
+            stage_5(BaseID, ReqID, {Vary, Func}, Opts);
+        not_found ->
+            ?event(
+                warning,
+                {key_resolver_not_found,
+                    {device, DeviceID},
+                    {key, Key}
+                },
+                Opts
+            ),
+            {error, not_found}
+    end.
+
+%% @doc Stage 5: Apply the `vary' function to the `BaseID' and `ReqID' to 
+%% load the arguments and validate their types for the `resolver' function.
+stage_5(BaseID, ReqID, {Vary, Func}, Opts) ->
+    {ok, VariedBase, VariedReq} = Vary(BaseID, ReqID, Opts),
+    stage_6(BaseID, Func, VariedBase, VariedReq, Opts).
+
+%% @doc Stage 6: Try to read the `VariedBase/VariedReq' from the cache. Return
+%% if found. If not found, we move on to the next stage. This deduplicates all
+%% prior computations for `Base` and `Req' messages that reduce to the same
+%% `Vary'ed versions.
+stage_6(BaseID, Func, VariedBase, VariedReq, Opts) ->
+    case hb_cache:read(HP = <<VariedBase/binary, "/", VariedReq/binary>>, Opts) of
+        not_found -> stage_7(BaseID, Func, VariedBase, VariedReq, Opts);
+        {ok, VariedResult} ->
+            % If the generic result upon the `VariedBase/VariedReq' key is found,
+            % we skip execution and jump to the final stage: normalizing the
+            % generic result to the specific `BaseID', if appropriate.
+            ?event(ao_core, {varied_cache_hit, {path, HP}}, Opts),
+            stage_9(BaseID, VariedResult, Opts)
+    end.
+
+%% @doc Stage 7: Execute the `resolver' function with the given arguments.
+stage_7(BaseID, Func, VariedBase, VariedReq, Opts) ->
+    Args = hb_ao_device:truncate_args(Func, [VariedBase, VariedReq, Opts]),
+    ?event(ao_core, {executing_resolver, {func, Func}, {args, Args}}, Opts),
+    case apply(Func, Args) of
+        {ok, RawResult} ->
+            ?event(
+                ao_core,
+                {resolver_execution_succeeded,
+                    {func, Func},
+                    {args, Args},
+                    {result, RawResult}},
+                Opts
+            ),
+            stage_8(BaseID, VariedBase, VariedReq, RawResult, Opts);
+        {error, Reason} ->
+            ?event(
+                ao_core,
+                {resolver_execution_failed,
+                    {func, Func},
+                    {args, Args},
+                    {reason, Reason}
+                },
+                Opts
+            ),
+            {error, Reason}
+    end.
+
+%% @doc Stage 8: Write the raw result to the cache and link it to the 
+%% `VariedBase/VariedReq' key. Future callers whose varied `Base' and `Req'
+%% reduce to the same `VariedBase/VariedReq' key will be able to read this
+%% result from the cache.
+stage_8(BaseID, VariedBase, VariedReq, RawResult, Opts) ->
+    {ok, ResultID} = hb_cache:write(RawResult, Opts),
+    ok =
+        hb_cache:link(
+            ResultID,
+            VariedHP = <<VariedBase/binary, "/", VariedReq/binary>>,
+            Opts
+        ),
+    ?event(
+        ao_core,
+        {wrote_result_to_cache, {varied_path, VariedHP}, {result, ResultID}},
+        Opts
+    ),
+    stage_9(BaseID, RawResult, Opts).
+
+%% @doc Stage 9: Replace `... : base` in the result with `... : OriginalBaseID'
+%% if present. This ensures that keys the resolver would like to passthrough
+%% are preserved in the result.
+stage_9(BaseID, Result = #{ '...' := base }, Opts) ->
+    ?event(
+        ao_core,
+        {returning_extended_result, {result, Result}, {base, BaseID}},
+        Opts
+    ),
+    {ok, Result#{ '...' => BaseID }};
+stage_9(_BaseID, Result, Opts) ->
+    ?event(
+        ao_core,
+        {returning_unmodified_result, {result, Result}},
+        Opts
+    ),
+    {ok, Result}.
