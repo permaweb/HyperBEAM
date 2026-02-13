caa1b9495b0529e022250c7434d9c7d423a75b33
speeddragon <speeddragon@gmail.com>
2026-02-13 16:35:21 +0000

    impr: Increase connection pool to 20 per peer

--- src/hb_http_client.erl
+++ src/hb_http_client.erl
@@ -21,7 +21,7 @@
 -define(CONN_COUNTER_ETS, hb_http_client_conn_counter).
 
 %% Connection pool sizes per type (easily configurable)
--define(READ_POOL_SIZE, 10).
+-define(READ_POOL_SIZE, 20).
 -define(WRITE_POOL_SIZE, 10).
 
 %%% ==================================================================
