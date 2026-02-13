145eec461233e5e57885084d985b961d545586bb
speeddragon <speeddragon@gmail.com>
2026-02-13 20:27:52 +0000

    fix: startup error

--- src/hb_store_arweave.erl
+++ src/hb_store_arweave.erl
@@ -182,7 +182,8 @@ init_prometheus() ->
                     {name, hb_store_arweave_index_check_duration_seconds},
                     {buckets, [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1]},
                     {help, "How much it takes to check the index"}
-                ])
+                ]),
+                ok
             catch
                 error:mfa_already_exists -> ok;
                 _:_ -> ok
