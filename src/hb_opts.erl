03026c52eef232c192368744a69f00ca65270f32
Ayush Agrawal <ayush.aa347@gmail.com>
2026-02-14 03:32:10 -0500

    rename eth-events@1.0 device to eth-client@1.0

--- src/hb_opts.erl
+++ src/hb_opts.erl
@@ -231,8 +231,7 @@ default_message() ->
             #{<<"name">> => <<"secret@1.0">>, <<"module">> => dev_secret},
             #{<<"name">> => <<"wasi@1.0">>, <<"module">> => dev_wasi},
             #{<<"name">> => <<"wasm-64@1.0">>, <<"module">> => dev_wasm},
-            #{<<"name">> => <<"whois@1.0">>, <<"module">> => dev_whois},
-            #{<<"name">> => <<"eth-events@1.0">>, <<"module">> => dev_eth_events}
+            #{<<"name">> => <<"whois@1.0">>, <<"module">> => dev_whois}
         ],
         %% Default execution cache control options
         cache_control => [<<"no-cache">>, <<"no-store">>],
