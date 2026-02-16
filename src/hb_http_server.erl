71ec0295483801b91f71539bbfcb2ef0a905dcad
Peter Farber <farberpete@gmail.com>
2026-02-16 15:00:21 -0500

    fix: remove tracer

--- src/hb_http_server.erl
+++ src/hb_http_server.erl
@@ -522,7 +522,7 @@ handle_request(RawReq, Body, ServerID) ->
                     {cowboy_req, {explicit, Req}, {body, {string, Body}}}
                 }
             ),
-            TracePID = hb_tracer:start_trace(),
+            % TracePID = hb_tracer:start_trace(),
             % Parse the HTTP request into HyerBEAM's message format.
             ReqSingleton =
                 try hb_http:req_to_tabm_singleton(Req, Body, NodeMsg)
@@ -541,16 +541,16 @@ handle_request(RawReq, Body, ServerID) ->
                 ?event(http,
                     {parsed_singleton,
                         {req_singleton, ReqSingleton},
-                        {accept_codec, CommitmentCodec}},
-                    #{trace => TracePID}
+                        {accept_codec, CommitmentCodec}}
+                    % #{trace => TracePID}
                 ),
                 % hb_tracer:record_step(TracePID, request_parsing),
                 % Invoke the meta@1.0 device to handle the request.
                 {ok, Res} =
                     dev_meta:handle(
                         NodeMsg#{
-                            commitment_device => CommitmentCodec,
-                            trace => TracePID
+                            commitment_device => CommitmentCodec
+                            % trace => TracePID
                         },
                         ReqSingleton
                     ),
