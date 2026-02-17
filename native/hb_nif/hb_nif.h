847951cd1752a6dc1f3c6fd8dee45555d5032380
JamesPiechota <piechota@gmail.com>
2026-02-17 10:23:52 -0500

    feat: bring over L1 ECDSA support from arweave (#640)

---
+++ native/hb_nif/hb_nif.h
@@ -0,0 +1,14 @@
+#ifndef HB_NIF_H
+#define HB_NIF_H
+
+#include <erl_nif.h>
+
+// Based on Arweave's c_src/ar_nif.h
+
+ERL_NIF_TERM solution_tuple(ErlNifEnv*, ERL_NIF_TERM);
+ERL_NIF_TERM ok_tuple(ErlNifEnv*, ERL_NIF_TERM);
+ERL_NIF_TERM ok_tuple2(ErlNifEnv*, ERL_NIF_TERM, ERL_NIF_TERM);
+ERL_NIF_TERM error_tuple(ErlNifEnv*, const char*);
+ERL_NIF_TERM make_output_binary(ErlNifEnv*, unsigned char*, size_t);
+
+#endif // HB_NIF_H
