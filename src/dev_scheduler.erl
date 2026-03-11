097b2d6bd2ac12914154bf9567aba109dde1bc57
Jack Frain <jfrain99@gmail.com>
2026-03-10 20:08:55 -0400

    fix: do not poison cache on remote sched load

--- src/dev_scheduler.erl
+++ src/dev_scheduler.erl
@@ -957,7 +957,8 @@ do_get_remote_schedule(ProcID, LocalAssignments, From, To, Redirect, Opts) ->
                 <<
                     ProcID/binary, "?process-id=", ProcID/binary,
                     FromBin/binary, ToParam/binary,
-                    "&limit=", (hb_util:bin(?MAX_ASSIGNMENT_QUERY_LEN))/binary
+                    "&limit=", (hb_util:bin(?MAX_ASSIGNMENT_QUERY_LEN))/binary,
+                    "&show-anchor"
                 >>
         end,
     ?event({getting_remote_schedule, {node, {string, Node}}, {path, {string, Path}}}),
