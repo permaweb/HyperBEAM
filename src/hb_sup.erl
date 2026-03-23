a7fa718b3bdf2b034c74848993903f8d50dce883
Victor Shyba <victor.shyba@gmail.com>
2026-03-23 14:49:35 -0300

    metrics: use a collector callback on hackney metrics

--- src/hb_sup.erl
+++ src/hb_sup.erl
@@ -32,16 +32,7 @@ init(Opts) ->
             type => worker,
             modules => [hb_http_client]
         },
-    HackneyMonitor =
-        #{
-            id => hb_hackney_monitor,
-            start => {hb_hackney_monitor, start_link, [Opts]},
-            restart => permanent,
-            shutdown => 5000,
-            type => worker,
-            modules => [hb_hackney_monitor]
-        },
-    {ok, {SupFlags, [GunChild, HackneyMonitor | StoreChildren]}}.
+    {ok, {SupFlags, [GunChild | StoreChildren]}}.
 
 %% @doc Generate a child spec for stores in the given Opts.
 store_children(Store) when not is_list(Store) ->
