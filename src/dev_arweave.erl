58efdfb0be1b97f7ec1e86937b59a32865991af0
speeddragon <speeddragon@gmail.com>
2026-03-26 18:30:45 +0000

    fix: 502 on raw request when trying to call non ID

--- src/dev_arweave.erl
+++ src/dev_arweave.erl
@@ -172,8 +172,7 @@ raw(Base, Request, Opts) ->
 head_raw(Base, Request, Opts) ->
     ?event(debug_raw, {raw, {base, Base}, {request, Request}}),
     case find_key(<<"raw">>, Base, Request, Opts) of
-        not_found -> {error, not_found};
-        TXID ->
+        TXID when ?IS_ID(TXID) ->
             % Read the data from the local cache.
             IndexStore = hb_store_arweave:store_from_opts(Opts),
             case hb_store_arweave:read_offset(IndexStore, TXID) of
@@ -197,7 +196,9 @@ head_raw(Base, Request, Opts) ->
                         Opts
                     ),
                     {error, not_found}
-            end
+            end;
+        _ -> 
+            {error, not_found}
     end.
 
 %% @doc Arweave transaction headers are not part of the Arweave data tree, and
