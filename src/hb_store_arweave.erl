700321744b53815bb0599d2a1a48e4fc8d8bd385
speeddragon <speeddragon@gmail.com>
2026-05-07 15:22:58 +0100

    impr: Fix bug on index scope

--- src/hb_store_arweave.erl
+++ src/hb_store_arweave.erl
@@ -13,11 +13,10 @@
 -export([write_block_item_ids/4, read_block_marker_depth/2]).
 -export([decode_item_ids/1, is_block_indexed/3, is_post_cutover/2, mark_block_indexed/3 ]).
 -include("include/hb.hrl").
+-include("include/hb_store_arweave.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 -define(PARTITION_SIZE, 3_600_000_000_000).
--define(SCOPE_PARENT, <<"parent">>).
--define(SCOPE_OFFSET, <<"offset">>).
 -define(CUTOVER_KEY, <<"block/marker-cutover-height">>).
 
 %% @doc Find the first Arweave store from the given node message. Searches first
