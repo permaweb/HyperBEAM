6a124583d3cfc9bb9cfd4bf83b26e59c8d1b0509
speeddragon <speeddragon@gmail.com>
2026-05-14 19:18:34 +0100

    impr: Add bundle id to task failure

--- src/dev_bundler.erl
+++ src/dev_bundler.erl
@@ -434,6 +434,7 @@ handle_task_failed(WorkerPID, Task, Reason, State = #state{
     ?event(
         bundler_short,
         dev_bundler_task:log_task(task_failed_retrying, Task, [
+            {bundle_id, {explicit, Task#task.bundle_id}},
             {retry_count, RetryCount},
             {delay_ms, Delay},
             {reason, {explicit, Reason}}
